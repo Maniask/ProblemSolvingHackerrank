2617f83ef57c37cb342e3f2e816dddb1e6ce1bdd
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2020-02-27 01:22:04 +0530

    Create Solution.java

---
+++ Plus Minus/Solution.java
@@ -0,0 +1,105 @@
+/*
+Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+For example, given the array  there are  elements, two positive, two negative and one zero. Their ratios would be ,  and . It should be printed as
+
+0.400000
+0.400000
+0.200000
+Function Description
+
+Complete the plusMinus function in the editor below. It should print out the ratio of positive, negative and zero items in the array, each on a separate line rounded to six decimals.
+
+plusMinus has the following parameter(s):
+
+arr: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers describing an array of numbers .
+
+Constraints
+
+
+
+Output Format
+
+You must print the following  lines:
+
+A decimal representing of the fraction of positive numbers in the array compared to its size.
+A decimal representing of the fraction of negative numbers in the array compared to its size.
+A decimal representing of the fraction of zeros in the array compared to its size.
+Sample Input
+
+6
+-4 3 -9 0 4 1         
+Sample Output
+
+0.500000
+0.333333
+0.166667
+Explanation
+
+There are  positive numbers,  negative numbers, and  zero in the array.
+The proportions of occurrence are positive: , negative:  and zeros: .
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        float p=0,q=0,r=0;
+        float z=arr.length;
+        int pcount=0,ncount=0,zcount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                pcount++;                
+            }
+            else if(arr[i]<0){
+                ncount++;                
+            }
+            else{
+                zcount++;                
+            }
+        }
+        float r1=pcount/z;
+        float r2=ncount/z;
+        float r3=zcount/z;
+
+        System.out.printf("%.6f \n",r1);
+        System.out.printf("%.6f \n",r2);
+        System.out.printf("%.6f",r3);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
